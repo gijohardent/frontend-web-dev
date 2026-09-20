6f98cfc5c2846fb42c632f7f9ae6e6aaa070a42d
gijohardent <s2200152@student.unklab.ac.id>
2026-09-20 13:11:00 +0800

    upload component & props

---
+++ materi/component_props/App.tsx
@@ -0,0 +1,15 @@
+// import UserComponent from "./component/UserComponent.tsx"
+
+// function App() {
+//   return (
+//     <>
+//       <UserComponent
+//         nama="John Doe"
+//         email="johndoe@gmail.com"
+//         nim={12345}
+//       />
+//     </>
+//   )
+// }
+
+// export default App
